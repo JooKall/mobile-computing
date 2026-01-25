a4559b09d9925fd482c81030d7ba560c6449596c
Joonas Kallio <kallio.joonas@proton.me>
2026-01-25 16:53:51 +0200

    HW3 database done

---
+++ HW3Database/app/src/main/java/com/example/hw3database/AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.hw3database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+// I found help here in the Singleton Pattern recommended by the android guide:
+// https://www.geeksforgeeks.org/kotlin/how-to-use-singleton-pattern-for-room-database-in-android/
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE =
+                        Room.databaseBuilder(context,AppDatabase::class.java, "database")
+                            .allowMainThreadQueries()
+                            .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
